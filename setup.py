6e02d9659afd293af1f1f338b05fd069a53a94c2
navidagz <navidagz76@gmail.com>
2020-05-07 16:34:46 +0430

    reformat code

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 import pathlib
+
 from setuptools import setup
 
-# The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
-# The text of the README file
 README = (HERE / "README.md").read_text()
 
-# This call to setup() does all the work
 setup(
     name="django-rest-api-logger",
     version="0.1",
